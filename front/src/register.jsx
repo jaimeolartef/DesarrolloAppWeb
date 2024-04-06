25a4e8a245ef5650467e0ba358580368a05bdf82
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-04-05 23:54:29 -0500

    Realiza el servicio rest en el back y modificaciones de campos obligatiros en front

--- front/src/register.jsx
+++ front/src/register.jsx
@@ -25,7 +25,13 @@ const Body = () => {
       correo
     };
 
-    await axios.post('/register/data', newUser);
+    const response = await axios.post('/register/data', newUser);
+
+    if (response.status === 200) {
+      alert('El registro ha sido exitoso');
+    } else {
+      alert('Hubo un error al registrar el usuario');
+    }
   }
 
   return (
@@ -35,37 +41,37 @@ const Body = () => {
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Tipo de Documento</Form.Label>
-          <Form.Select aria-label="Seleccione el tipo de documento" value={tipoDocumento} onChange={e => setTipoDocumento(e.target.value)}>
+          <Form.Select aria-label="Seleccione el tipo de documento" value={tipoDocumento} onChange={e => setTipoDocumento(e.target.value)} required>
             <option>Seleccione el tipo de documento</option>
-            <option value="cc">Cédula de Ciudadanía</option>
-            <option value="pasaporte">Pasaporte</option>
-            <option value="ce">Cédula Extranjería</option>
+            <option value="Cédula de Ciudadanía">Cédula de Ciudadanía</option>
+            <option value="Pasaporte">Pasaporte</option>
+            <option value="Cédula Extranjería">Cédula Extranjería</option>
           </Form.Select>
         </Form.Group>
         <br/>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Documento</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese el documento" value={documento} onChange={e => setDocumento(e.target.value)}/>
+          <Form.Control type="text" placeholder="Ingrese el documento" value={documento} onChange={e => setDocumento(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese el nombre" value={nombre} onChange={e => setNombre(e.target.value)}/>
+          <Form.Control type="text" placeholder="Ingrese el nombre" value={nombre} onChange={e => setNombre(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Fecha de Nacimiento</Form.Label>
-          <Form.Control type="date" placeholder="Ingrese la fecha de nacimiento" value={fechaNacimiento} onChange={e => setFechaNacimiento(e.target.value)}/>
+          <Form.Control type="date" placeholder="Ingrese la fecha de nacimiento" value={fechaNacimiento} onChange={e => setFechaNacimiento(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Teléfono</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese el teléfono" value={telefono} onChange={e => setTelefono(e.target.value)}/>
+          <Form.Control type="text" placeholder="Ingrese el teléfono" value={telefono} onChange={e => setTelefono(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Correo Electrónico</Form.Label>
-          <Form.Control type="email" placeholder="Ingrese el correo electrónico" value={correo} onChange={e => setCorreo(e.target.value)}/>
+          <Form.Control type="email" placeholder="Ingrese el correo electrónico" value={correo} onChange={e => setCorreo(e.target.value)} required/>
         </Form.Group>
         <br></br>
         <Button variant="primary" type="submit">
--- server.js
+++ server.js
@@ -1,33 +1,57 @@
-import ex from  'express';
-import dir from 'path';
+import ex from 'express'
+import dir from 'path'
 import data from './json/consult.json' assert { type: 'json' }
+import fs from 'fs'
 
-const app = ex()
-const dir_front = dir.resolve()
-app.use(ex.static("front/build"))
+const app = ex();
+const dir_front = dir.resolve();
+app.use(ex.static("front/build"));
+app.use(ex.json());
 
 
-app.listen('8881', function() {
+app.listen('8881', function () {
     console.log("Server started")
     console.log(dir_front)
 })
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(dir_front + "/front/build/index.html")
 })
 
-app.get('/register', function(req, res){    
+app.get('/register', function (req, res) {
     res.sendFile(dir_front + "/front/build/index.html")
 })
 
-app.get('/consult', function(req, res){
+app.get('/consult', function (req, res) {
     res.sendFile(dir_front + "/front/build/index.html")
 })
 
-app.get('/consult/data', function(req, res){
+app.get('/consult/data', function (req, res) {
     res.json(data)
 })
 
-app.post('/register/data', function(req, res){    
-        console.log('aqui entro')
-})
+app.post('/register/data', function (req, res) {
+    const user = req.body;
+    console.log(user);
+    // Synchronous read
+    const data = JSON.parse(fs.readFileSync(dir.resolve('./json/consult.json'), 'utf8'));
+
+    // Asynchronous read
+    fs.readFile(dir.resolve('./json/consult.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file from disk: ${err}`);
+        } else {
+            const jsonData = JSON.parse(data);
+            jsonData.push(user);
+            fs.writeFile(dir.resolve('./json/consult.json'), JSON.stringify(jsonData), 'utf8', (err) => {
+                if (err) {
+                    console.error(`Error writing file: ${err}`);
+                } else {
+                    console.log('File is written successfully!');
+                }
+            });
+        }
+    });
+
+    res.sendStatus(200);
+});
